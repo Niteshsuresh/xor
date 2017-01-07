b85c77108703dc9aeb5b84e7176827eb32f095d4
Nitesh <nitesh@pratian.com>
2017-01-07 18:08:54 +0530

    adding xor classification and regression methods

--- brain.py
+++ brain.py
@@ -1,10 +1,17 @@
- from pybrain.datasets.supervised import SupervisedDataSet
+ from pybrain.datasets import SupervisedDataSet, ClassificationDataSet
  from pybrain.tools.shortcuts import buildNetwork
  from pybrain.supervised.trainers import BackpropTrainer
 
  if __name__ == "__main__":
-    net = buildNetwork(2, 3, 1)
-     ds = SupervisedDataSet(2, 1)
+   #regression
+   #net = buildNetwork(2, 3, 1)
+   #ds = SupervisedDataSet(2, 1)
+	 
+	 #classification
+	 net = buildNetwork(12, hidden_nodes, 3, outclass=SoftmaxLayer, hiddenclass=TanhLayer)
+     ds = ClassificationDataSet(2, 1, nb_classes=4)
+  
+  
      ds.addSample((1,0),(1,))
      ds.addSample((0,1),(1,))
      ds.addSample((0,0),(0,))
@@ -12,4 +19,3 @@
      trainer = BackpropTrainer(net, ds)
      trainer.train();
      print "\nResult: ", round(net.activate([0,0]))
-
